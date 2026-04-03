e7b21119b69f3e5ca7c8f60909c3e64bbd33be27
hzr <805756350@qq.com>
2026-04-03 12:43:05 +0800

    n数求和完善

--- n_num2.cpp
+++ n_num2.cpp
@@ -3,10 +3,10 @@
 
 using namespace std;
 
-const int N = 4096;
-const int REPEAT = 10;   
+const int N = 65536;
+const int REPEAT = 50;   
 
-double a[N], sum, sumcmp[4] = {0, 0, 0, 0};
+double a[N], sum, sumcmp[6] = {0, 0, 0, 0, 0 ,0};
 
 void init(int N) {
     sum = 0;
@@ -39,7 +39,9 @@ void recursion2(int N) {
 bool check() {
     return sumcmp[0] == sumcmp[1] &&
            sumcmp[1] == sumcmp[2] &&
-           sumcmp[2] == sumcmp[3];
+           sumcmp[2] == sumcmp[3] &&
+           sumcmp[3] == sumcmp[4] &&
+           sumcmp[4] == sumcmp[5];
 }
 
 double test1() {
@@ -109,16 +111,73 @@ double test4() {
     return (tail.QuadPart - head.QuadPart) * 1000.0 / freq.QuadPart / REPEAT;
 }
 
+double test5() {
+    LARGE_INTEGER head, tail, freq;
+    QueryPerformanceFrequency(&freq);
+
+    QueryPerformanceCounter(&head);
+    for (int t = 0; t < REPEAT; t++) {
+        init(N);
+        double sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
+        for (int i = 0; i < N; i += 4) {
+            sum1 += a[i];
+            sum2 += a[i + 1];
+            sum3 += a[i + 2];
+            sum4 += a[i + 3];
+        }
+        sum = sum1 + sum2 + sum3 + sum4;
+        sumcmp[4] = sum;
+    }
+    QueryPerformanceCounter(&tail);
+
+    return (tail.QuadPart - head.QuadPart) * 1000.0 / freq.QuadPart / REPEAT;
+}
+
+double test6() {
+    LARGE_INTEGER head, tail, freq;
+    QueryPerformanceFrequency(&freq);
+
+    QueryPerformanceCounter(&head);
+    for (int t = 0; t < REPEAT; t++) {
+        init(N);
+        double sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0, sum6 = 0,sum7 = 0, sum8 = 0;
+        for (int i = 0; i < N; i += 8) {
+            sum1 += a[i];
+            sum2 += a[i + 1];
+            sum3 += a[i + 2];
+            sum4 += a[i + 3];
+            sum5 += a[i + 4];
+            sum6 += a[i + 5];
+            sum7 += a[i + 6];
+            sum8 += a[i + 7];
+        }
+        sum = sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7 + sum8;
+        sumcmp[5] = sum;
+    }
+    QueryPerformanceCounter(&tail);
+
+    return (tail.QuadPart - head.QuadPart) * 1000.0 / freq.QuadPart / REPEAT;
+}
+
 int main() {
     double t1 = test1();
     double t2 = test2();
     double t3 = test3();
     double t4 = test4();
-
+    double t5 = test5();
+    double t6 = test6();
     cout << "Method 1 time: " << t1 << " ms" << endl;
     cout << "Method 2 time: " << t2 << " ms" << endl;
     cout << "Method 3 time: " << t3 << " ms" << endl;
     cout << "Method 4 time: " << t4 << " ms" << endl;
+    cout << "Method 5 time: " << t5 << " ms" << endl;
+    cout << "Method 6 time: " << t6 << " ms" << endl;
+
+    cout<<"method 2 " << (t1/t2) << "x faster than method 1" << endl;
+    cout<<"method 3 " << (t1/t3) << "x faster than method 1" << endl;
+    cout<<"method 4 " << (t1/t4) << "x faster than method 1" << endl;
+    cout<<"method 5 " << (t1/t5) << "x faster than method 1" << endl;
+    cout<<"method 6 " << (t1/t6) << "x faster than method 1" << endl;
 
     if (check()) {
         cout << "All methods give the same result." << endl;
